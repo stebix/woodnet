8895697162c619393acf60e34e92b252286ae3ad
Jannik Stebani <jannik.stebani@physik.uni-wuerzburg.de>
2024-03-27 02:32:19 +0100

    add testing for compiled model

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -4,6 +4,7 @@ the histogram parameter logging.
 
 @jsteb 2024
 """
+import torch
 import pytest
 
 from woodnet.models.volumetric import ResNet3D
@@ -12,8 +13,12 @@ from woodnet.logtools.tensorboard.modelparameters.extraction import (extract_sim
 
 
 def test_manual_extraction():
+    print(torch.__version__)
     net = ResNet3D(in_channels=1)
-    parameters, _ = extract_simple_resnet_parameters(net)
+
+    net_fast = torch.compile(net)
+
+    parameters, _ = extract_simple_resnet_parameters(net_fast)
     parameters = convert_to_flat(parameters)
 
     for key, value in parameters.items():
