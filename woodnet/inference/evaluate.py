19e3cfbd82f48694a0d5093e7ac262ee74b89be6
Jannik Stebani <jannik@lrmreko.lrmreko.physik.uni-wuerzburg.de>
2024-04-16 18:43:46 +0200

    reintroduce predictor class into module

--- woodnet/inference/evaluate.py
+++ woodnet/inference/evaluate.py
@@ -1,5 +1,6 @@
 """
-Implements functional interface to evaluate the model in the context of robustness experiments.
+Implements functional interface and predictor class to evaluate the model in the context
+of robustness experiments.
 
 High-level overview:
 
@@ -23,7 +24,9 @@ instances/checkpoints are expected to be bundled together.
 """
 import logging
 import dataclasses
+import contextlib
 from collections.abc import Mapping, Sequence
+
 
 import torch
 import torch.utils
@@ -33,7 +36,6 @@ from woodnet.datasets.volumetric_inference import set_parametrized_transform
 from woodnet.evaluation.metrics import compute_cardinalities
 from woodnet.inference.inference import get_transforms_name, maybe_wrap_loader
 from woodnet.inference.parametrized_transforms import CongruentParametrizations, ParametrizedTransform
-from woodnet.inference.predictor import Predictor
 from woodnet.trackers import TrackedCardinalities
 
 
@@ -83,6 +85,96 @@ def evaluate(model: torch.nn.Module,
 
         results.append({'parameters' : parametrization.parameters, 'metrics' : tracker.state_dict()})
     return results
+
+
+
+class Predictor:
+
+    def __init__(self,
+                 model: torch.nn.Module,
+                 device: str | torch.device,
+                 dtype: torch.dtype,
+                 use_amp: bool,
+                 use_inference_mode: bool,
+                 display_transforms_progress: bool,
+                 display_parametrizations_progress: bool,
+                 display_loader_progress: bool,
+                 leave_transforms_progress: bool = True
+                 ) -> None:
+
+        self.model = model
+        self.device = torch.device(device) if isinstance(device, str) else device
+        self.dtype = dtype
+        self.use_amp = use_amp
+        self.use_inference_mode = use_inference_mode
+        self.display_transforms_progress = display_transforms_progress
+        self.display_parametrizations_progress = display_parametrizations_progress
+        self.display_loader_progress = display_loader_progress
+        self.leave_transforms_progress = leave_transforms_progress
+
+
+    def disabled_gradient_context(self):
+        if self.use_inference_mode:
+            return torch.inference_mode()
+        return torch.no_grad()
+
+
+    def amp_context(self):
+        return torch.autocast(device_type=self.device.type, enabled=self.use_amp)
+
+
+    def inference_contexts(self) -> tuple:
+        """Get configured no-gradient and mixed precision contexts."""
+        return (self.disabled_gradient_context(), self.amp_context())
+
+
+    def configure_model(self) -> None:
+        """Jointly adjust model float precision, eval mode and testing flag."""
+        self.model.eval()
+        logger.debug('Set model to evaluation mode.')
+        self.model.testing = True
+        final_nonlinearity = getattr(self.model, 'final_nonlinearity', None)
+        logger.debug(
+            f'Set model testing flag. Final nonlinearity is: {final_nonlinearity}'
+        )
+        self.model.to(dtype=self.dtype, device=self.device)
+        logger.debug(f'Moved model to precision {self.dtype} and device {self.device}')
+
+
+    def predict(self,
+                loader: torch.utils.data.DataLoader,
+                transforms: Sequence[CongruentParametrizations]) -> dict:
+        """
+        Perform full prediction run (full loader) for a number of parmetrizations
+
+        Results are reported as a metadata-enhanced dictionary with
+        """
+        report = []
+        self.configure_model()
+
+        if self.display_transforms_progress:
+            settings = {'desc' : 'transformations', 'unit' : 'tf',
+                        'leave' : self.leave_transforms_progress}
+            transforms = tqdm.tqdm(transforms, **settings)
+
+        with contextlib.ExitStack() as stack:
+            # conditionally use amp and inference or no-grad mode for speed/throughput
+            [stack.enter_context(cm) for cm in self.inference_contexts()]
+            for parametrizations in transforms:
+                if self.display_transforms_progress:
+                    transforms.set_postfix_str(f'transform=\'{parametrizations.name}\'')
+
+                result = evaluate(self.model,
+                                  loader=loader, parametrizations=parametrizations,
+                                  device=self.device, dtype=self.dtype,
+                                  display_parametrizations_progress=self.display_transforms_progress,
+                                  display_loader_progress=self.display_loader_progress)
+
+                parametrization_report = {'metadata' : parametrizations.info(), 'report' : result}
+                report.append(parametrization_report)
+
+        return report
+
 
 
 
