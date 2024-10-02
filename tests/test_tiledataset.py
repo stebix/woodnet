54f27d522151023906bc905af45ba019581c9ffd
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2024-10-02 03:34:00 +0200

    add proper test

--- tests/test_tiledataset.py
+++ tests/test_tiledataset.py
@@ -5,61 +5,26 @@ from woodnet.transformations.transformer import Transformer
 from woodnet.transformations import from_configurations
 
 
-def test():
-
+def test_smoke_builder():
     phase = 'train'
     tileshape = (256, 256, 256)
     configs = [
-        {'name' : 'Normalize3D', 'mean' : 110, 'std' : 950}
+        {'name' : 'Normalize', 'mean' : 110, 'std' : 950}
     ]
-
-
+    IDs = ['CT10', 'CT20']
     builder = TileDatasetBuilder()
 
-    datasets = builder.build('CT10', 'CT20', 'CT10',
+    datasets = builder.build(IDs,
                              phase=phase, tileshape=tileshape,
                              transform_configurations=configs)    
 
-    for dataset in datasets:
-        print('dataset str repr :: ', dataset)
-        index = 12
-        item, label = dataset[index]
-        print(dataset.fingerprint)
-        print('length is :: ', len(dataset))
-        print(f'label is {label}')
-        print(item.shape)
-        print(type(item))
+    assert len(datasets) == 2
 
-    return None
+    # CT10 should be acer, CT20 should be pinus
+    ds_acer, ds_pinus = datasets
+    item = next(iter(ds_acer))
 
-    path = '/home/jannik/storage/wood/custom/CT10.zarr'
-    phase = 'train'
-    tileshape = (256, 256, 256)
-    configs = [
-        {'name' : 'Normalize3D', 'mean' : 110, 'std' : 950}
-    ]
-    classlabel_mapping = {'ahorn' : 0, 'kiefer' : 1}
-
-    transformer = Transformer(*from_configurations(configs))
-
-    dataset = TileDataset(
-        path=path, phase=phase, tileshape=tileshape, transformer=transformer,
-        classlabel_mapping=classlabel_mapping
-    )
-
-    print(dataset)
-
-    element, label = dataset[2]
-    print(f'label = {label}')
-    print(type(element))
-    print(f'shape = {element.shape}')
-    print(f'mean = {torch.mean(element)}')
-
-
-
-def main():
-    test()
-
-
-if __name__ == '__main__':
-    main()
+    # deeply inspect structures of returned data
+    assert isinstance(item, tuple)
+    subvolume, label = item
+    assert label.item() == 0, 'acer should be class 0'
