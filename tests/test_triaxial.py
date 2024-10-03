de1304a32db9f171a996d78c977dc49d0f2f8522
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2024-10-03 20:12:35 +0200

    move fixture scaffolding to conftest for reusability

--- tests/test_triaxial.py
+++ tests/test_triaxial.py
@@ -1,75 +1,15 @@
 from pathlib import Path
 
-import numpy as np
 import pytest
-import zarr
-import zarr.convenience
 
 from woodnet.datasets.triaxial import TriaxialDatasetBuilder
-from woodnet.datasets.setup import InstanceFingerprint
-
-
-BASE_DATA_SHAPE: tuple[int, int, int, int] = (1, 128, 128, 128)
-INTERNAL_PATH: str = 'group/data'
-
-
-@pytest.fixture(scope='module')
-def tempdir(tmp_path_factory):
-    return tmp_path_factory.mktemp('mock-raw-data')
-
-
-def create_zarr_dataset(directory: Path, classname: str) -> Path:
-    """
-    Create an authentic zarr dataset with a fingerprint inside the
-    given directory. The dataset will be created with a random
-    data array of shape BASE_DATA_SHAPE.
-    """
-    data = np.random.default_rng().normal(0, 1, size=BASE_DATA_SHAPE)
-    fingerprint = {
-        'class_': classname,
-        'voltage': '40kV',
-        'current': '200muA',
-        'duration': '1s',
-        'averages': 2
-    }
-    zarr_path = directory / f'{classname}-testdataset.zarr'
-    group, dataset = INTERNAL_PATH.split('/')
-    # create internal structure and write data
-    array = zarr.open(zarr_path, mode='w')
-    group = array.create_group(group)
-    dataset = group.create_dataset(dataset, data=data)
-    # write metadata fingerprint
-    for k, v in fingerprint.items():
-        array.attrs[k] = v
-    
-    return zarr_path
 
 
 
-@pytest.fixture(scope='module')
-def mock_instance_mapping(tempdir):
-    class_a = 'acer'
-    class_b = 'pinus'
-    instance_mapping_raw = {
-        'picard' : {
-            'location' : str(create_zarr_dataset(tempdir, class_a)),
-            'classname' : class_a,
-            'group' : 'axial'
-        },
-        'kirk' : {
-            'location' : str(create_zarr_dataset(tempdir, class_b)),
-            'classname' : class_b,
-            'group' : 'tangential'
-        }
-    }
-    instance_mapping = {k : InstanceFingerprint(**v) for k, v in instance_mapping_raw.items()}
-    return instance_mapping
-
-
-def test_builder(mock_instance_mapping):
+def test_builder(mock_instance_mapping, internal_path):
     # patch builder for local test-wise data configuration
     TriaxialDatasetBuilder.instance_mapping = mock_instance_mapping
-    TriaxialDatasetBuilder.internal_path = INTERNAL_PATH
+    TriaxialDatasetBuilder.internal_path = internal_path
     TriaxialDatasetBuilder.classlabel_mapping = {
         'acer' : 1701,
         'pinus' : 2893
@@ -91,10 +31,10 @@ def test_builder(mock_instance_mapping):
 
 
 @pytest.mark.parametrize('tileshape', [(64, 64, 64), (32, 32, 32)])
-def test_provides_correct_orthoplane_shape(mock_instance_mapping, tileshape):
+def test_provides_correct_orthoplane_shape(mock_instance_mapping, internal_path, tileshape):
     # patch builder for local test-wise data configuration
     TriaxialDatasetBuilder.instance_mapping = mock_instance_mapping
-    TriaxialDatasetBuilder.internal_path = INTERNAL_PATH
+    TriaxialDatasetBuilder.internal_path = internal_path
     TriaxialDatasetBuilder.classlabel_mapping = {
         'acer' : 1701,
         'pinus' : 2893
