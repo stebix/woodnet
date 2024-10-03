727ee739a013ce3af4c32aa636922a47baebcc66
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2024-10-03 23:28:40 +0200

    improve testing coverage

--- tests/test_ScoredCheckpoint.py
+++ tests/test_ScoredCheckpoint.py
@@ -1,12 +1,14 @@
 import pathlib
 import pytest
+import logging
 
 from woodnet.checkpoint import ScoredCheckpoint, excise_optimal_qualifier
 from woodnet.checkpoint.scores import ScoreRank
+from woodnet.checkpoint.handlers import SEP
 
 @pytest.fixture
 def testfilepath(tmp_path) -> pathlib.Path:
-    fpath = tmp_path / 'model-optimal-testfile.pth'
+    fpath = tmp_path / f'model{SEP}optimal{SEP}testfile.pth'
     with fpath.open(mode='w') as handle:
         handle.write('Hello World from Pytest!')
     return fpath
@@ -19,7 +21,7 @@ def test_initialization(testfilepath):
     print(checkpoint)
 
 
-def test_remove_file(testfilepath):
+def test_remove_checkpoint(testfilepath):
     assert testfilepath.is_file(), 'file must exist at beginning: test setup failure'
     score = 0.9 # ssio
     rank = ScoreRank.FEASIBLE
@@ -28,7 +30,7 @@ def test_remove_file(testfilepath):
     assert not testfilepath.exists(), 'should not exist after remove method call'
 
 
-def test_demote_file(testfilepath):
+def test_demote_checkpoint(testfilepath):
     assert testfilepath.is_file(), 'file must exist at beginning: test setup failure'
     score = 0.9 # ssio
     rank = ScoreRank.OPTIMAL
@@ -40,15 +42,51 @@ def test_demote_file(testfilepath):
     assert not testfilepath.exists(), 'should not exist after demotion method call'
 
 
+def test_demote_checkpoint_with_nonoptimal_rank(testfilepath, caplog):
+    assert testfilepath.is_file(), 'file must exist at beginning: test setup failure'
+    score = 0.9 # ssio
+    rank = ScoreRank.FEASIBLE
+    checkpoint = ScoredCheckpoint(score=score, filepath=testfilepath, rank=rank)
+    checkpoint.demote()
+
+    # a little bit of a hacky and brittle way to check that we emitted a warning
+    # about the attempted demotion of a non-optimal instance
+    record = caplog.records[-1]
+    assert 'attempted demotion of non-optimal instance' in record.message
+    assert checkpoint.rank == ScoreRank.FEASIBLE
+    assert testfilepath.exists(), 'should still exist after demotion method call'
+
+
+def test_demote_checkpoint_with_nonexciseable_qualifier(tmp_path, caplog):
+    fpath = tmp_path / f'picard-is%top_captain.pth'
+
+    with fpath.open(mode='w') as handle:
+        handle.write('Hello World from Pytest!')
+
+    score = 0.9 # ssio
+    rank = ScoreRank.OPTIMAL
+    checkpoint = ScoredCheckpoint(score=score, filepath=fpath, rank=rank)
+    checkpoint.demote()
+
+    # a little bit of a hacky and brittle way to check that we emitted a warning
+    # about the attempted demotion of a non-optimal instance
+    record = caplog.records[-1]
+    assert 'Qualifier excision from filename failed' in record.message
+    assert checkpoint.rank == ScoreRank.OPTIMAL, 'since demotion failed, rank should not have changed'
+    assert fpath.exists(), 'should still exist after demotion method call'
+
+
 
 class Test_excise_optimal_parameter:
+    # we decided upon a fixed seperator string for the paths
+    # this must be observed for buidling the test and expected paths  
 
     def test_with_well_formed_path(self):
         testpath = pathlib.Path(
-            '/nested/directories/fold-1/chkpt-optimal-wildUUID.pth'
+            f'/nested/directories/fold-1/chkpt{SEP}optimal{SEP}wildUUID.pth'
         )
         expected_result = pathlib.Path(
-            '/nested/directories/fold-1/chkpt-wildUUID.pth'
+            f'/nested/directories/fold-1/chkpt{SEP}wildUUID.pth'
         )
         result = excise_optimal_qualifier(testpath)
         assert result == expected_result
@@ -56,10 +94,10 @@ class Test_excise_optimal_parameter:
 
     def test_with_separated_but_unusual_qualifier(self):
         testpath = pathlib.Path(
-            '/nested/directories/fold-1/chkpt-pessimal-wildUUID.pth'
+            f'/nested/directories/fold-1/chkpt{SEP}pessimal{SEP}wildUUID.pth'
         )
         expected_result = pathlib.Path(
-            '/nested/directories/fold-1/chkpt-wildUUID.pth'
+            f'/nested/directories/fold-1/chkpt{SEP}wildUUID.pth'
         )
         result = excise_optimal_qualifier(testpath)
         assert result == expected_result
