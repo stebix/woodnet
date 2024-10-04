eef357f6ddc7f2f1bb156ca3e6d837d79c3ec964
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2024-10-04 20:25:22 +0200

    add functionality to export instance mapping to array for cv splitting

--- woodnet/datasets/setup.py
+++ woodnet/datasets/setup.py
@@ -3,8 +3,9 @@ Central location for entire data setup.
 
 @Jannik Stebani 2024
 """
+from collections import defaultdict
 from pathlib import Path
-from typing import Any
+from typing import Literal
 from pydantic import BaseModel, field_validator, ValidationError
 
 from woodnet.custom.exceptions import ConfigurationError
@@ -59,19 +60,82 @@ def load_data_configuration(filepath: Path) -> DataConfiguration:
 
 
 
-def group_instances_by_class(instance_mapping: dict[str, InstanceFingerprint]) -> dict[str, list[str]]:
-    """
-    Group instances by class name.
-    """
-    class_to_instances: dict[str, str] = {}
+def _group_by_class_detailed(instance_mapping: dict[str, InstanceFingerprint]
+                             ) -> dict[str, dict[str, InstanceFingerprint]]:
+    class_to_instances = defaultdict(dict)
     for ID, fingerprint in instance_mapping.items():
-        if fingerprint.classname not in class_to_instances:
-            class_to_instances[fingerprint.classname] = []
+        class_to_instances[fingerprint.classname][ID] = fingerprint    
+    return class_to_instances
+
+
+def _group_by_class_concise(instance_mapping: dict[str, InstanceFingerprint]
+                            ) -> dict[str, list[str]]:
+    class_to_instances = defaultdict(list)
+    for ID, fingerprint in instance_mapping.items():
         class_to_instances[fingerprint.classname].append(ID)
     return class_to_instances
+
+
+def group_instances_by_class(instance_mapping: dict[str, InstanceFingerprint],
+                             format: Literal['list', 'mapping'] = 'list'
+                             ) -> dict[str, list[str]]:
+    """
+    Group instances by class name.
+
+    Parameters
+    ----------
+
+    instance_mapping : dict[str, InstanceFingerprint]
+        Mapping of instance IDs to instance fingerprints.
+
+    format : Literal['list', 'mapping']
+        Format of the output.
+        If 'list', the output will be a concise list of instance IDs for each class.
+        If 'mapping' the output will be a dictionary with instance IDs as keys
+        and remaining fingerprint data as dictionay values.
+
+    Returns
+    -------
+
+    class_to_instances : dict[str, list[str]] | dict[str, dict[str, InstanceFingerprint]]
+    """
+    if format == 'list':
+        return _group_by_class_concise(instance_mapping)
+    elif format == 'mapping':
+        return _group_by_class_detailed(instance_mapping)
+    else:
+        raise ValueError(f'Invalid format \'{format}\'. Use \'list\' or \'mapping\'.')
     
 
 DATA_CONFIGURATION = load_data_configuration(DATA_CONFIGURATION_PATH)
 INSTANCE_MAPPING = DATA_CONFIGURATION.instance_mapping
 INTERNAL_PATH = DATA_CONFIGURATION.internal_path
 CLASSLABEL_MAPPING = DATA_CONFIGURATION.class_to_label_mapping
+
+
+from typing import NamedTuple
+
+class InstanceMappingLists(NamedTuple):
+    IDs: list[str]
+    CLASSES: list[str]
+    GROUPS: list[str]
+
+
+def convert_to_lists(instance_mapping: dict[str, InstanceFingerprint]) -> dict[str, list[str]]:
+    """Convert the nested mapping to three flat lists
+    
+    The instance mapping maps instance IDs to instance fingerprints, which in turn
+    possess information about the data location on the filesystem, the class name
+    and the group of the instance. This function converts the nested mapping to three lists
+    where the information at an index is related to the same instance.
+    """
+    IDS = []
+    CLASSES = []
+    GROUPS = []
+
+    for ID, fingerprint in instance_mapping.items():
+        IDS.append(ID)
+        CLASSES.append(fingerprint.classname)
+        GROUPS.append(fingerprint.group)
+
+    return InstanceMappingLists(IDS, CLASSES, GROUPS)
