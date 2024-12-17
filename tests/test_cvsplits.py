980d44cc5613be898cfc787851c9250d2a9ff534
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2024-12-17 03:44:48 +0100

    fix: skip deprecated tests

--- tests/test_cvsplits.py
+++ tests/test_cvsplits.py
@@ -1,16 +1,38 @@
 import rich
 import pytest
 
-from woodnet.datasets.setup import INSTANCE_MAPPING, group_instances_by_class, convert_to_lists
+from woodnet.datasets.setup import group_instances_by_class, convert_to_lists, InstanceFingerprint
 
-from woodnet.cvsplits import INSTANCES, CLASSES, WOOD_CLASSES, ORIENTATION_ClASSES, remap_group
+from woodnet.cvsplits import INSTANCES, WOOD_CLASSES, remap_group, StratifiedKFoldsGenerator
 
-from woodnet.cvsplits import StratifiedKFoldsGenerator
+from tests.scaffolding.syntheticdata import (ClassSpecification,
+                                             create_data_configuration, create_instance_informations)
+
+@pytest.mark.skip
+def test(monkeypatch):
+
+    specs = [
+        ClassSpecification(name='acer', label=0, groups={'red', 'green'}, instances_per_group=3),
+        ClassSpecification(name='pinus', label=1, groups={'blue', 'green'}, instances_per_group=3),
+    ]
+    data_configuration = create_data_configuration(specs, base_location='path/to/data', internal_path='group/dset')
+    instance_mapping = {k: InstanceFingerprint(**v) for k, v in data_configuration['instance_mapping'].items()}
+
+    instance_by_class = group_instances_by_class(instance_mapping, format='mapping')
+
+    instances, classes, groups = convert_to_lists(instance_mapping)
+
+    monkeypatch.setattr('woodnet.cvsplits.INSTANCES', instances)
+    monkeypatch.setattr('woodnet.cvsplits.WOOD_CLASSES', classes)
+    monkeypatch.setattr('woodnet.cvsplits.ORIENTATION_CLASSES', groups)
 
 
+    generator = StratifiedKFoldsGenerator()
+    r = generator[1]
+    
+    rich.print(r)
 
-def test():
-    instance_by_class = group_instances_by_class(INSTANCE_MAPPING, format='mapping')
+    raise KeyError
 
     nid, ncl, ngr = convert_to_lists(INSTANCE_MAPPING)
 
