eb1b96f1ebe913578018841829d322ed206d8b42
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2024-12-17 01:43:51 +0100

    refactor synthetic dataset creation

--- tests/scaffolding/syntheticdata.py
+++ tests/scaffolding/syntheticdata.py
@@ -20,6 +20,7 @@ import zarr
 
 import pytest
 
+from woodnet.datasets.setup import InstanceFingerprint
 
 PathLike = str | Path
 logger = logging.getLogger()
@@ -118,8 +119,87 @@ def materialize_instance(ID: str,
     logger.info(f'Created synthetic dataset instance at \'{location.resolve()}\'')
     return location
 
+Shape4D = tuple[int, int, int, int]
+
+INTERNAL_PATH: str = 'downsampled/half'
+RAW_DIMENSIONS: Shape4D = (1, 320, 320, 320)
+BINARY_CLASS_SPEC: list[ClassSpecification] = [
+    ClassSpecification(name='acer', label=0, groups={'red', 'green'}, instances_per_group=1),
+    ClassSpecification(name='pinus', label=1, groups={'blue', 'green'}, instances_per_group=1),
+]
 
 
+
+@dataclass
+class SyntheticDatasetPackage:
+    """
+    Joint information and data for a synthetic
+    dataset intended for testing purposes.
+    """
+    internal_path: str
+    base_directory: Path
+    instance_mapping: dict[str, dict]
+    data_configuration: dict[str, Any]
+
+
+class SyntheticDatasetGenerator:
+    default_internal_path: str = INTERNAL_PATH
+    default_raw_dimensions: Shape4D = RAW_DIMENSIONS
+    default_class_specification: list[ClassSpecification] = BINARY_CLASS_SPEC
+
+    def __init__(self,
+                 internal_path: str,
+                 raw_dimensions: Shape4D,
+                 specs: Sequence[ClassSpecification],
+                 cast_fingerprints: bool = True) -> None:
+        self.internal_path = internal_path
+        self.raw_dimensions = raw_dimensions
+        self.specs = specs
+        self.cast_fingerprints = cast_fingerprints
+    
+    @classmethod
+    def from_defaults(cls) -> 'SyntheticDatasetGenerator':
+        """Generate instance with sensible defaults for binary classification."""
+        return cls(internal_path=cls.default_internal_path,
+                   raw_dimensions=cls.default_raw_dimensions,
+                   specs=cls.default_class_specification)
+    
+
+    def generate(self, base_directory: str | Path) -> SyntheticDatasetPackage:
+        """Generate the test data on the file system."""
+        data_configuration = create_data_configuration(
+            self.specs,
+            base_directory,
+            internal_path=self.internal_path
+        )
+        instance_mapping: dict = data_configuration['instance_mapping']
+        for ID, instance in instance_mapping.items():
+            materialize_instance(ID, instance, self.raw_dimensions, self.internal_path)
+
+        if self.cast_fingerprints:
+            instance_mapping = {
+                k : InstanceFingerprint(**v) for k, v in instance_mapping.items()
+            }
+
+        kwargs = {
+            'internal_path' : self.internal_path, 'base_directory' : base_directory,
+            'instance_mapping' : instance_mapping, 'data_configuration' : data_configuration
+        }
+        return SyntheticDatasetPackage(**kwargs)
+
+
+
+
+@pytest.fixture(scope='session')
+def synthetic_dataset(tmp_path_factory) -> dict[str, Any]:
+    """Create a full synthetic dataset in a temporary directory."""
+    test_dataset_directory = tmp_path_factory.mktemp('synthetic_datasets')
+    generator = SyntheticDatasetGenerator.from_defaults()
+    info_bundle = generator.generate(test_dataset_directory)
+    return info_bundle
+
+
+'''
 @pytest.fixture(scope='session')
 def synthetic_dataset(tmp_path_factory) -> dict[str, Any]:
     """Create a full synthetic dataset in a temporary directory."""
@@ -142,3 +222,4 @@ def synthetic_dataset(tmp_path_factory) -> dict[str, Any]:
         'data_configuration' : data_configuration
     }
     return info_bundle
+'''
