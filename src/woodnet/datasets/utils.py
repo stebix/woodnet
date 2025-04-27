7d0f60daa8fb7f12629a4dd45e82901c0195f7ec
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-04-27 16:03:52 +0200

    feature: add statistics computation and easier ROI

--- src/woodnet/datasets/utils.py
+++ src/woodnet/datasets/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from pathlib import Path
 
+import skimage.morphology as morph
+
 from woodnet.datasets.constants import (COMPLETE_DATASET_DIRECTORY,
                                         CLASS_ID_ORIENTATION_MAPPING)
 
@@ -33,3 +35,51 @@ def get_ID_by(class_: str, orientation: None | str = None) -> list[str]:
     else:
         IDs = list(IDs_mapping.keys())
     return IDs
+
+
+def compute_statistics(
+    volume: np.ndarray,
+    mask: np.ndarray | None = None
+) -> dict:
+    inshape = volume.shape
+    voxelcount = volume.size
+    if mask is not None:
+        volume = volume[mask]
+    statistics = {}
+    # Cast to float to make the values JSON-serializable.
+    # 32 bit numpy floats are not natively JSON-serializable
+    statistics['minimum'] = float(np.min(volume))
+    statistics['maximum'] = float(np.max(volume))
+    statistics['mean'] = float(np.mean(volume))
+    statistics['median'] = float(np.median(volume))
+    statistics['stdev'] = float(np.std(volume))
+    statistics['total_voxelcount'] = voxelcount
+    statistics['roi_voxelcount'] = np.count_nonzero(mask) if mask is not None else voxelcount
+    statistics['shape'] = inshape
+    statistics['q_95']  = float(np.quantile(volume, q=0.95))
+    statistics['q_05']  = float(np.quantile(volume, q=0.05))
+    statistics['q_99']  = float(np.quantile(volume, q=0.99))
+    statistics['q_01']  = float(np.quantile(volume, q=0.01))
+    return statistics
+
+
+def generate_cylindrical_roi(
+    shape: tuple[int, ...],
+    *,
+    dtype: np.dtype = bool,
+    ) -> np.ndarray:
+    try:
+        *pre, z_sz, y_sz, x_sz = shape
+    except ValueError:
+        pre = ()
+        z_sz, y_sz, x_sz = shape
+    if not y_sz == x_sz:
+        raise ValueError(f'cannot create cylindrical ROI for non-square shape with {x_sz=} and {y_sz=}')
+    
+    disk = morph.disk(radius=x_sz//2)
+    if disk.shape != (y_sz, x_sz):
+        disk = disk[:-1, :-1]
+    assert disk.shape == (y_sz, x_sz), 'wat'
+    disk = np.broadcast_to(disk[np.newaxis, ...], shape=(z_sz, y_sz, x_sz))
+    disk = np.expand_dims(disk, axis=tuple(i for i in range(len(pre)))).astype(dtype)
+    return disk
