6c5e7b82742b9fe60ef78593e6f1c41b4e7b7909
stebix <jannik.stebani@physik.uni-wuerzburg.de>
2025-05-02 02:00:21 +0200

    fix: proper type hint and add string repr

--- src/woodnet/datasets/pipelining/pipeline.py
+++ src/woodnet/datasets/pipelining/pipeline.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from woodnet.datasets.pipelining.base import PipelineStep
+from woodnet.datasets.pipelining.arrayseqmap import ArraySequence
 
 class Pipeline:
 
@@ -10,10 +11,24 @@ class Pipeline:
     ) -> None:
         self.steps = steps
 
-    def __call__(self, data: np.ndarray) -> np.ndarray:
+    def __call__(self, data: np.ndarray | ArraySequence) -> np.ndarray | ArraySequence:
         """
         Apply the pipeline to the data.
         """
         for step in self.steps:
             data = step(data)
         return data
+    
+    def __repr__(self) -> str:
+        format_string = self.__class__.__name__
+        format_string += '(\n'
+        format_string += f'  step_count={len(self.steps)},\n'
+        format_string +=  '  steps=\n    ['
+        for step in self.steps:
+            format_string += '\n'
+            format_string += f'        {step}'
+        format_string += '\n    ]\n)'
+        return format_string
+
+    def __str__(self) -> str:
+        return repr(self)
